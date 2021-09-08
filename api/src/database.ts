e271d7c0e6de8b619b6efc0b8bae16cef8074435
gabriel <15620759+genrique98@users.noreply.github.com>
2021-09-07 18:33:07 -0700

    style: update

--- api/src/database.ts
+++ api/src/database.ts
@@ -1,53 +1,19 @@
 import pg from 'pg';
 import dotenv from 'dotenv';
+import * as c from './config/config'
 
 dotenv.config()
 
 const { Pool } = pg;
 
-const { 
-    POSTGRES_HOST, 
-    POSTGRES_DB, 
-    POSTGRES_TEST_DB, 
-    POSTGRES_USER, 
-    POSTGRES_PASSWORD,
-    ENV 
-} = process.env;
-
-// let config = {};
 const config = {
-    host: POSTGRES_HOST,
-    database: POSTGRES_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD
+    host: c.config.host,
+    database: c.config.database,
+    user: c.config.username,
+    password: c.config.password,
+    port: c.config.port
 }
 
 const Client = new Pool(config);
-
-
-
-
-// if (ENV === 'test') {
-//     config = {
-//         host: POSTGRES_HOST,
-//         database: POSTGRES_TEST_DB,
-//         user: POSTGRES_USER,
-//         password: POSTGRES_PASSWORD
-//     }
-//     Client = new Pool(config);
-
-// }
-// if (ENV === 'dev') {
-//     config = {
-//         host: POSTGRES_HOST,
-//         database: POSTGRES_DB,
-//         user: POSTGRES_USER,
-//         password: POSTGRES_PASSWORD
-//     }
-//     Client = new Pool(config);
-
-// }
-
-// let Client = new Pool(config);
 
 export default Client
