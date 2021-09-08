7e1fd7640351fe9436f4118683ce8d7d99b44fe1
gabriel <15620759+genrique98@users.noreply.github.com>
2021-09-07 18:35:31 -0700

    style fix

--- api/src/middleware/auth.ts
+++ api/src/middleware/auth.ts
@@ -1,16 +1,16 @@
 import express, { Request, Response, NextFunction } from 'express'
 import jwt, { Secret } from 'jsonwebtoken'
+import * as c from '../config/config'
 
 const verifyAuthToken = (req: Request, res: Response, next: NextFunction):void => {
     try {
-        const { TOKEN_SECRET } = process.env;
         const authorizationHeader: string = (req.headers.authorization as unknown) as string;
         const token = authorizationHeader.split(' ')[1]
-        const decoded = jwt.verify(token, TOKEN_SECRET as Secret)
-
+        const decoded = jwt.verify(token, c.config.token_secret as Secret)
         next()
     } catch (error) {
         res.status(401)
+        res.json('Access denied, invalid token')
     }
 }
 
