8df8fc6b0b3e3ae8d5892cd8550c731c1dcb31b0
gabriel <15620759+genrique98@users.noreply.github.com>
2021-09-22 18:33:11 -0700

    feat: fetch data from eb api

--- frontend/src/app/services/product/product.service.ts
+++ frontend/src/app/services/product/product.service.ts
@@ -3,6 +3,8 @@ import { Product } from 'src/app/models/Product';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http'
 import * as data from 'src/assets/data.json';
+// import * as dotenv from "dotenv";
+// dotenv.config();
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +14,7 @@ export class ProductService {
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<Product[]>{
-    // return (data.default as unknown) as Product[];
-    return this.http.get<Product[]>("assets/data.json");
+    const api: string = "http://backend-env.eba-gpma9uyx.us-west-1.elasticbeanstalk.com/products" ;//process.env.PRODUCTS_API_URL as unknown as string;
+    return this.http.get<Product[]>(api);
   }
 }
