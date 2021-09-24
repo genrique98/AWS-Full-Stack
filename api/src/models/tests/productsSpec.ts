1d5aefea01b27dc7f7cde4235bda73efec6a871c
gabriel <15620759+genrique98@users.noreply.github.com>
2021-09-23 19:34:17 -0700

    test: deleted unnecessary tests

--- api/src/models/tests/productsSpec.ts
+++ api/src/models/tests/productsSpec.ts
@@ -7,37 +7,9 @@ describe("Product Model", () => {
     expect(store.index).toBeDefined();
   });
 
-  it('should have a show method', () => {
-    expect(store.show).toBeDefined();
-  });
-
-  it('should have a create method', () => {
-    expect(store.create).toBeDefined();
-  });
-
-  it('create method should add a product', async () => {
-    const numOfProducts = await (await store.index()).length;
-
-    await store.create({
-      name: 'book',
-      price: 10,
-      url: 'www.url.com',
-      description: 'this is a description'
-    });
-
-    const newNumOfProducts = await (await store.index()).length;
-
-    expect(newNumOfProducts).toBe(numOfProducts + 1);
-  });
-
   it('index method should return a list of products', async () => {
     const result = await store.index();
     expect(result).toBeTruthy();
-  });
-
-  it('show method should return the correct product', async () => {
-    const result = await store.show("1");
-    expect(result).toEqual(result)
   });
   
 });
