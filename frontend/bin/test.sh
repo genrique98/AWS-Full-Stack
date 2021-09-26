60deaa598a3dc9104e54e37ca878bdd26d62c7eb
gabriel <15620759+genrique98@users.noreply.github.com>
2021-09-25 23:08:07 -0700

    karma test

--- frontend/bin/test.sh
+++ frontend/bin/test.sh
@@ -1,2 +1,2 @@
-env CHROME_BIN=/usr/bin/google-chrome 
-ng test #--browsers=Headless --no-watch
+env | grep CHROME_BIN=/usr/bin/google-chrome 
+ng test --browsers=ChromeHeadless #--browsers=Headless --no-watch
