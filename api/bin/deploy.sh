9dcf398f4ca82b13429103a9c074cc05644a2cd2
gabriel <15620759+genrique98@users.noreply.github.com>
2021-09-28 18:34:18 -0700

    chore: set env variables from circleci

--- api/bin/deploy.sh
+++ api/bin/deploy.sh
@@ -1,2 +1,15 @@
 eb use backend-env
+
+eb setenv NAME=$NAME
+eb setenv BCRYPT_PASSWORD=$BCRYPT_PASSWORD
+eb setenv ENV=$ENV
+eb setenv PORT=$PORT
+eb setenv RDS_DB_NAME=$RDS_DB_NAME
+eb setenv RDS_HOSTNAME=$RDS_HOSTNAME
+eb setenv RDS_PASSWORD=$RDS_PASSWORD
+eb setenv RDS_PORT=$RDS_PORT
+eb setenv RDS_USERNAME=$RDS_USERNAME
+eb setenv SALT_ROUNDS=$SALT_ROUNDS
+eb setenv TOKEN_SECRET=$TOKEN_SECRET
+
 eb deploy backend-env
